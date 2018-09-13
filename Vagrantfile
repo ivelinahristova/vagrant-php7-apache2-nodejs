9156344f50b95022a1665de2c572d8ab15b244a9
Ivelina Hristova <ivelinahhristova@gmail.com>
2018-09-13 10:49:11 +0300

    Update php, nodejs, git; Add composer and xdebug;

--- Vagrantfile
+++ Vagrantfile
@@ -1,30 +1,19 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# All Vagrant configuration is done below. The "2" in Vagrant.configure
-# configures the configuration version (we support older styles for
-# backwards compatibility). Please don't change it unless you know what
-# you're doing.
 Vagrant.configure(2) do |config|
-  # The most common configuration options are documented and commented below.
-  # For a complete reference, please see the online documentation at
-  # https://docs.vagrantup.com.
-
-  # Every Vagrant development environment requires a box. You can search for
-  # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "ubuntu/trusty32"
+  config.vm.box = "ubuntu/trusty64"
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.network :forwarded_port, guest: 80, host: 5588
-  config.vm.network :forwarded_port, guest: 5533, host: 5533
-  config.vm.network "public_network"
+  config.vm.network :forwarded_port, guest: 80, host: 5511
+  config.vm.network :forwarded_port, guest: 5544, host: 5544
+  #config.vm.network "public_network", bridge: "..."
+  config.vm.network :private_network, ip: "192.168.68.11"
   config.vm.provider "virtualbox" do |v|
 	  v.memory = 1024
 	end
 	
-config.ssh.username = 'vagrant'
-config.ssh.password = 'vagrant'
-#config.ssh.insert_key = 'true'
-
+  config.ssh.username = 'vagrant'
+  config.ssh.password = 'vagrant'
 
   config.ssh.insert_key = false
   config.ssh.paranoid = false
--- bootstrap.sh
+++ bootstrap.sh
@@ -12,16 +12,24 @@ add-apt-repository ppa:ondrej/php
 apt-get -y update
 apt-get -y upgrade
 
-apt-get install -y php7
+apt-get install -y php7.2
 
 apt-get install apache2
 
 echo -e "\n--- Installing PHP-specific packages ---\n"
-sudo apt-get install -y --force-yes php7.0-common php7.0-dev php7.0-json php7.0-opcache php7.0-cli libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-mcrypt php7.0-mbstring php7.0-bcmath php7.0-zip
+sudo apt-get install -y --force-yes php7.2-xdebug php7.2-memcached php7.2-pgsql php7.2-common php7.2-dev php7.2-json php7.2-opcache php7.2-cli libapache2-mod-php7.2 php7.2 php7.2-mysql php7.2-fpm php7.2-curl php7.2-gd php7.2-mcrypt php7.2-mbstring php7.2-bcmath php7.2-zip
 echo -e "\n--- Installed PHP packages ---\n"
 php -m
 
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get -y --force-yes install php-pear
+
+echo -e "\n--- Installing Git ---\n"
+sudo apt-get install -y --force-yes git unzip
+echo -e "\n--- Installing Git Finish ---\n"
+echo -e "\n--- Git Path: ---\n"
+sudo which git
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get -y --force-yes install nodejs
 
 echo -e "\n--- Node JS Version: "
@@ -34,6 +42,23 @@ echo -e "\n"
 npm install --global gulp-cli
 echo -e "\n--- Gulp Version: "
 gulp -v
+
+echo -e "\n--- Installing Composer ---\n"
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer
+
+# Xdebug configuration
+for f in /etc/php/7.2/mods-available/xdebug.ini
+do
+  echo "xdebug.remote_enable=true" >> $f
+  echo "xdebug.remote_connect_back=true" >> $f
+  echo "xdebug.idekey=MY_AWESOME_KEY" >> $f
+done
+
 
 if ! [ -L /var/www/html ]; then
   rm -rf /var/www/html
